a70c7a947ced274836d19ef7f6c10c23745b897e
kjungyoun <rlawjddbs963@naver.com>
2021-04-28 05:19:52 +0900

    04.27 HomeWork Spring

--- 0427/hw0427/src/main/java/com/ssafy/guestbook/model/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.guestbook.model.service;
-
-import java.util.Map;
-
-import com.ssafy.guestbook.model.MemberDto;
-
-public interface MemberService {
-	public MemberDto login(Map<String, String> map) throws Exception;
-}
